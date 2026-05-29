6ea2d3bc4af1451ce6cb14f105afcaa5efaf774c
Ignacy Pękała <ignacy@pekala.eu>
2026-05-29 23:48:00 +0200

    Add valgrind check

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 make polynomials > /dev/null
 
+valgrind=$1
+
 passed=0
 failed=0
 for batch in tests/* ; do
@@ -8,10 +10,15 @@ for batch in tests/* ; do
         test_name=${base_path#tests/$batch/}
         output_file=$base_path.out
         if ! diff <(./polynomials < $input_file) $output_file -u --color; then
-            echo "Test $test_name failed"
+            echo Test $test_name failed
             ((failed++))
         else
-            ((passed++))
+            if [[ $valgrind != "off" ]] && ! valgrind -q --error-exitcode 1 ./polynomials < $input_file > /dev/null; then
+                echo Valgrind reported errors in $test_name
+                ((failed++))
+            else
+                ((passed++))
+            fi
         fi
     done
 done
